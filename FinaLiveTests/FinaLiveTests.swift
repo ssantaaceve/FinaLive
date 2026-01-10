21806d231710e6126f1076ac6da952dc7ce8e37d
Sergio Andres Santa Acevedo <sergiosanta970617@gmail.com>
2026-01-09 20:22:15 -0500

    Initial Commit

---
+++ FinaLiveTests/FinaLiveTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinaLiveTests.swift
+//  FinaLiveTests
+//
+//  Created by Sergio Andres  Santa Acevedo on 9/1/2026.
+//
+
+import Testing
+@testable import FinaLive
+
+struct FinaLiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
